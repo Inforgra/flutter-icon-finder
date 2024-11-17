a035e3513388207828d79b9362020adfe60ce844
Inforgra <inforgrax@gmail.com>
2024-11-17 13:49:06 +0900

    screen, widget refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/screens/icons_screen.dart';
+import 'package:flutter_icons/screens/icon_viewer_screen.dart';
+import 'package:flutter_icons/widgets/icon_viewer.dart';
 import 'package:flutter_icons/widgets/theme_button.dart';
 import 'package:flutter_icons/widgets/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -32,52 +33,7 @@ class MainApp extends StatelessWidget {
         useMaterial3: true,
         brightness: Brightness.dark,
       ),
-      home: const Home(title: appTitle),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  final String title;
-  const Home({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        leading: Builder(builder: (context) {
-          return IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
-          );
-        }),
-        actions: const [
-          ThemeButton(),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Material Icons'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            )
-          ],
-        ),
-      ),
-      body: const IconsScreen(),
+      home: const IconViewerScreen(title: appTitle),
     );
   }
 }
--- lib/widgets/icon_viewer.dart
+++ lib/widgets/icon_viewer.dart
@@ -1,41 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/contants/material_icons.dart';
+import 'package:flutter_icons/contants/material_icons_outlined.dart';
+import 'package:flutter_icons/contants/material_icons_rounded.dart';
+import 'package:flutter_icons/contants/material_icons_sharp.dart';
+import 'package:flutter_icons/widgets/icon_list.dart';
 
-class IconViewer extends StatelessWidget {
-  final Map<String, IconData> icons;
-  final String keyword;
+class IconViewer extends StatefulWidget {
+  const IconViewer({super.key});
 
-  const IconViewer({super.key, required this.icons, required this.keyword});
+  @override
+  State<StatefulWidget> createState() {
+    return IconViewerState();
+  }
+}
 
-  int getCrossAxisAlignment(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    if (width < 640) {
-      return 4;
-    } else if (width < 768) {
-      return 4;
-    } else if (width < 1024) {
-      return 6;
-    } else if (width < 1280) {
-      return 8;
-    } else if (width < 1536) {
-      return 8;
-    } else {
-      return 8;
-    }
+enum IconType { base, outlined, rounded, sharp }
+
+class IconViewerState extends State<IconViewer> {
+  var currentKeyword = '';
+  var currentIconType = IconType.base;
+  var currentIcons = materialIconsDefault;
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void setKeyword() {
+    var text = _textEditingController.text;
+    setState(() {
+      currentKeyword = text;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController.addListener(setKeyword);
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: getCrossAxisAlignment(context),
-      children: icons.entries
-          .where((e) => e.key.contains(keyword))
-          .map((icon) => Column(
-                children: [
-                  Expanded(child: FittedBox(child: Icon(icon.value))),
-                  SizedBox(height: 32, child: Text(icon.key)),
-                ],
-              ))
-          .toList(),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: _textEditingController,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.search),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  _textEditingController.clear();
+                },
+                icon: const Icon(Icons.clear),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        SegmentedButton<IconType>(
+          segments: const [
+            ButtonSegment(
+              value: IconType.base,
+              label: Text('default'),
+              icon: Icon(Icons.radio_button_unchecked),
+            ),
+            ButtonSegment(
+              value: IconType.outlined,
+              label: Text('outlined'),
+              icon: Icon(Icons.radio_button_unchecked),
+            ),
+            ButtonSegment(
+              value: IconType.rounded,
+              label: Text('rounded'),
+              icon: Icon(Icons.radio_button_unchecked),
+            ),
+            ButtonSegment(
+              value: IconType.sharp,
+              label: Text('sharp'),
+              icon: Icon(Icons.radio_button_unchecked),
+            ),
+          ],
+          selected: <IconType>{currentIconType},
+          onSelectionChanged: (iconType) {
+            setState(() {
+              currentIconType = iconType.first;
+              switch (currentIconType) {
+                case IconType.base:
+                  currentIcons = materialIconsDefault;
+                  break;
+                case IconType.outlined:
+                  currentIcons = materialIconsOutlined;
+                  break;
+                case IconType.rounded:
+                  currentIcons = materialIconsRounded;
+                  break;
+                case IconType.sharp:
+                  currentIcons = materialIconSharp;
+                  break;
+              }
+            });
+          },
+        ),
+        const SizedBox(height: 16.0),
+        Expanded(
+          child: IconList(
+            icons: currentIcons,
+            keyword: currentKeyword,
+          ),
+        ),
+      ],
     );
   }
 }
